e9fa9fe2b3f5d24d42af0d18789bfda84e806aef
José Henrique <jose.henrique.ivan@gmail.com>
2021-09-14 19:54:20 -0300

    conserto do kasino

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import os
 import discord
 import time
+import asyncio
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='/')
 
 TOKEN = os.environ['token']
 
 client = discord.Client()
 
-
 @client.event
 async def on_ready():
 	print('Logged in as {0.user}!'.format(client))
-
 
 @client.event
 async def on_message(message):
@@ -18,20 +20,51 @@ async def on_message(message):
 		return
 
 	if message.content.startswith('kasino'):
-		voice_channel = message.author.voice.channel
+		voice_channel = message.author.voice
 
 		if voice_channel != None:
+			voice_channel = voice_channel.channel
 			vc = await voice_channel.connect()
 			vc.play(discord.FFmpegPCMAudio(source="sabadaco.mp3"))
-			
+
 			# Sleep while audio is playing.
 			while vc.is_playing():
-				time.sleep(.1)
+				await asyncio.sleep(0.3)
 			await vc.disconnect()
-		else:
-			await message.send(str(message.author.name) + "is not in a channel.")
 
 		# Delete command after the audio is done playing.
-		await message.message.delete()
+		await message.delete()
+
+	if message.content.startswith('shake it'):
+		voice_channel = message.author.voice
+
+		if voice_channel != None:
+			voice_channel = voice_channel.channel
+			vc = await voice_channel.connect()
+			vc.play(discord.FFmpegPCMAudio(source="shake it.mp3"))
+
+			# Sleep while audio is playing.
+			while vc.is_playing():
+				await asyncio.sleep(0.3)
+			await vc.disconnect()
+
+		# Delete command after the audio is done playing.
+		await message.delete()
+
+	if message.content.startswith('jet music'):
+		voice_channel = message.author.voice
+
+		if voice_channel != None:
+			voice_channel = voice_channel.channel
+			vc = await voice_channel.connect()
+			vc.play(discord.FFmpegPCMAudio(source="jet music.mp3"))
+
+			# Sleep while audio is playing.
+			while vc.is_playing():
+				await asyncio.sleep(0.3)
+			await vc.disconnect()
+
+		# Delete command after the audio is done playing.
+		await message.delete()
 
 client.run(TOKEN)
